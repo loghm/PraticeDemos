963405e0e6da32f77183fba026addd45f1aaec13
loghm <869276489@qq.com>
2021-05-13 09:56:00 +0800

    【1像素问题】注释

--- singleLineProject/singleLineProject/ViewController.m
+++ singleLineProject/singleLineProject/ViewController.m
@@ -28,6 +28,7 @@
     // 我们拿non-retina屏幕来举例，我们要绘制一个1个点宽度的线，因为non-retina屏中，1个点等于1个像素。因为绘制是按照，比如(1.0, 1.0)到(1.0, 10.0)的位置进行绘制，这样一个点宽度就会处在两个像素的中间，横跨2个像素，因为反锯齿的算法作用下，宽度会像外进行扩散，颜色变浅，导致1个点(1像素)的宽度，被绘制成了2个像素。这个时候只要偏移1/[UIScreen mainScreen].scale/2个点，就可以落在一个完整的像素宽度内，也就不会触发反锯齿的算法。
     // 可以通过(int)(1 * pixelScale + 1) % 2 == 0来判断是否落在奇数单元中(也就是宽度在两个像素中间)
     // see also: https://www.jianshu.com/p/d9f5e0aabcdb
+    // 官方文档：https://developer.apple.com/library/archive/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html
     
     UIView* vw1 = [[UIView alloc] initWithFrame:CGRectMake(20.500000, 200.500000, 80.000000, 40.00000)];
     [vw1.layer setBorderColor:[UIColor greenColor].CGColor];
